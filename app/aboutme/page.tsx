c5502b36c1320c2d74e9032e21a4c22d2e5349e5
MånsSchütz <mans.schutz@puzzel.com>
2025-06-05 11:45:02 +0200

    Test AboutMe component to include type definitions for search parameters and import CSS module

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,4 +1,14 @@
-export default function AboutMe() {
+import classes from '@/app/aboutme/page.module.css';
+
+type SearchParams = {
+  test?: string;
+};
+
+export default async function AboutMe(props: {
+	searchParams?: Promise<SearchParams>;
+}) {
+  const searchParams = await props.searchParams;
+  console.log(searchParams);
   return (
     <>
       <h1>About me</h1>
