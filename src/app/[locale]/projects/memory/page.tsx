b862cc121bd51a0df218c214a47957375945fd38
MansS <mans.schutz@puzzel.com>
2025-08-06 09:47:06 +0200

    feat: implement memory game functionality with card flipping and difficulty selection; add styles for memory grid and cards

--- src/app/[locale]/projects/memory/page.tsx
+++ src/app/[locale]/projects/memory/page.tsx
@@ -1,12 +1,104 @@
+'use client';
+
 import Link from "next/link";
 import classes from "@/src/app/[locale]/projects/memory/page.module.css";
+import { useEffect, useState } from "react";
+
+type Card = {
+    card: number;
+    id: number;
+};
+
+// TODO: Implement a second card flip state or something similar to track the second card flipped.
 
 export default function MemoryPage() {
+    const [difficultyMode, setDifficultyMode] = useState<boolean>(false);
+    const [deck, setDeck] = useState<number[]>([]);
+    const [firstCardFlipped, setFirstCardFlipped] = useState<Card | null>(null);
+    const [secondCardFlipped, setSecondCardFlipped] = useState<Card | null>(null);
+    const [matched, setMatched] = useState<number[]>([]);
+    const [triesLeft, setTriesLeft] = useState<number>(10);
+
+    function startNewGame() {
+        setDifficultyMode(true);
+    }
+
+    function resetGame() { }
+
+    function endGame() { }
+
+    function flipCard(card: number, id: number) {
+        console.log(`Flipped card: ${card} with id: ${id}`);
+        if (firstCardFlipped) {
+            setSecondCardFlipped({ card, id });
+            checkMatch(firstCardFlipped, { card, id });
+        } else {
+            setFirstCardFlipped({ card, id });
+        }
+
+    }
+
+    function checkMatch(firstCardFlipped: Card, secondCardFlipped: Card) {
+        console.log(`Checking match: ${firstCardFlipped.card} with ${secondCardFlipped.card}`);
+        if (firstCardFlipped.card === secondCardFlipped.card) {
+            setMatched(prev => [...prev, firstCardFlipped.id, secondCardFlipped.id]);
+            setFirstCardFlipped(null);
+            setSecondCardFlipped(null);
+        }
+        else {
+            setTimeout(() => {
+                setFirstCardFlipped(null);
+                setSecondCardFlipped(null);
+                setTriesLeft(triesLeft - 1);
+            }, 1000);
+        }
+        console.log(matched);
+
+    }
+
+    function generateDeck(nrOfCards: number) {
+        setDifficultyMode(false);
+        const nrCardsInRow: number = Math.sqrt(nrOfCards * 2);
+        console.log(nrCardsInRow);
+        setDeck(Array.from({ length: nrOfCards }, (_, i) => i % (nrOfCards / 2)).sort(() => Math.random() - 0.5));
+    }
+
     return (
         <>
             <h1>Memory</h1>
             <p>This is the Memory page.</p>
             <Link className={classes.githubLink} target="_blank" href="https://github.com/Althona/Trafic-help-Javascript">Github</Link>
+
+            <button onClick={startNewGame}>Start New game</button>
+
+            {difficultyMode &&
+                <>
+                    <p>Select difficulty</p>
+                    <button onClick={() => generateDeck(18)}>Easy Mode</button>
+                    <button onClick={() => generateDeck(36)}>Normal Mode</button>
+                    <button onClick={() => generateDeck(72)}>Hard Mode</button>
+                    <button onClick={() => (generateDeck(144))}>Insane Mode</button>
+                </>
+            }
+            {deck.length > 0 &&
+                <>
+                    <p>{`Tries Left: ${triesLeft}`}</p>
+                    <div className={classes.memoryGrid}>
+                        {deck.map((card, index) => (
+                            matched.includes(index) || firstCardFlipped?.id === index || secondCardFlipped?.id === index
+                                ?
+                                <div key={index} className={classes.memoryCard}>
+                                    <span className={classes.card}>{card}</span>
+                                </div>
+                                :
+                                <div key={index} className={classes.memoryCard}>
+                                    <span id={index.toString()} className={classes.card} onClick={() => flipCard(card, index)}>?</span>
+                                </div>
+                        ))}
+                    </div>
+                </>
+            }
+
         </>
     );
 }
